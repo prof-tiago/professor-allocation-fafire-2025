63bda10db69253bcec53d600f372d0d0b0cb3b70
Tiago Santos <professor.tiago.dos.santos@gmail.com>
2025-03-04 22:09:28 -0300

    Create project

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationApplicationFafire2025.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationApplicationFafire2025 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationApplicationFafire2025.class, args);
+	}
+
+}
