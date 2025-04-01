d811cacd25caa9649a8cff3ef38068844f9f4e38
Tiago Santos <professor.tiago.dos.santos@gmail.com>
2025-03-31 22:03:05 -0300

    Add @NoArgsConstructor annotation to entities

--- src/main/java/com/project/professor/allocation/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -9,9 +9,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Professor {
 
